449804db4e4b526da8bdcc79601a52431933cc54
aldi5883 <97704401+aldi5883@users.noreply.github.com>
2022-03-31 14:17:54 -0600

    l

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function windowLoaded() {
   console.log('loaded ok!');
-  document.getElementsById('button-1').classList.toggle('was-clicked');
+  document.getElementById('button-1').classList.toggle('was-clicked');
 }
 
 window.onload = windowLoaded;
