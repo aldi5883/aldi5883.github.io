887935dbbac1ae23971570c9037f6958c5c3aa13
aldi5883 <97704401+aldi5883@users.noreply.github.com>
2022-03-31 14:21:46 -0600

    k

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-
+function button1Clicked() {
+  document.getElementById('button-1').classList.toggle('was-clicked');
+}
 
 function windowLoaded() {
   console.log('loaded ok!');
-  document.getElementById('button-1').classList.toggle('was-clicked');
+  document.getElementById('button-1').addEventListener('click, button1Clicked');
+  /*
+  mouseover
+  mouseleave
+  mousedown
+  mouseup
+  */
 }
 
 window.onload = windowLoaded;
