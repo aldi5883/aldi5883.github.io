3e70f51cce93e3f48eb3f145fc8bd240fe827811
aldi5883 <97704401+aldi5883@users.noreply.github.com>
2022-05-02 16:37:13 -0600

    k

--- js/jsproj.js
+++ js/jsproj.js
@@ -1,17 +1,42 @@
-// portfolio
- $('.gallery ul li a').click(function() {
-     var itemID = $(this).attr('href');
-     $('.gallery ul').addClass('item_open');
-     $(itemID).addClass('item_open');
-     return false;
- });
- $('.close').click(function() {
-     $('.port, .gallery ul').removeClass('item_open');
-     return false;
- });
+<script>
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
 
- $(".gallery ul li a").click(function() {
-     $('html, body').animate({
-         scrollTop: parseInt($("#top").offset().top)
-     }, 400);
- });
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
